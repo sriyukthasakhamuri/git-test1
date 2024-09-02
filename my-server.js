16cc96267689d4e1121fee6051bfeedffc61399c
Sriyuktha Sakhamuri <2000030967@Kluniversity.in>
2024-09-02 00:23:12 -0400

    modified port

--- my-server.js
+++ my-server.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World from NodeJS!');
   res.end();
-}).listen(8000, function(){
+}).listen(3000, function(){
  console.log("Server running at port 8000");
 });
